7ede0fb50f2ac2807a00ca3c1b33f3cbc045e2a0
VPresich <valya.alexandrova.presich@gmail.com>
2025-12-05 18:56:58 +0100

    Add change theme in UserSettings form

--- src/components/UI/RadioGroup/RadioGroup.jsx
+++ src/components/UI/RadioGroup/RadioGroup.jsx
@@ -1,32 +1,67 @@
+// import clsx from "clsx";
+// import { Controller, useFormContext } from "react-hook-form";
+// import css from "./RadioGroup.module.css";
+
+// const RadioGroup = ({ name, options }) => {
+//   const { control } = useFormContext();
+
+//   return (
+//     <Controller
+//       name={name}
+//       control={control}
+//       render={({ field }) => (
+//         <div className={css.radioGroup}>
+//           {options.map((option) => (
+//             <label key={option} className={css.radioLabel}>
+//               <input
+//                 type="radio"
+//                 value={option}
+//                 checked={field.value === option}
+//                 onChange={() => field.onChange(option)}
+//                 className={clsx(css.radioInput, css[option])}
+//               />
+
+//               <span className={clsx(css.customRadio, css[option])}></span>
+//             </label>
+//           ))}
+//         </div>
+//       )}
+//     />
+//   );
+// };
+
+// export default RadioGroup;
+
 import clsx from "clsx";
-import { useSelector } from "react-redux";
-import { selectTheme } from "../../../redux/auth/selectors";
-import { useFormContext, Controller } from "react-hook-form";
+import { Controller, useFormContext } from "react-hook-form";
 import css from "./RadioGroup.module.css";
 
 const RadioGroup = ({ name, options }) => {
   const { control } = useFormContext();
-  const theme = useSelector(selectTheme);
 
   return (
     <Controller
       name={name}
       control={control}
       render={({ field }) => (
-        <div className={css.radioGroup}>
-          {options.map((option, index) => (
-            <label key={index} className={css.radioLabel}>
-              <input
-                type="radio"
-                value={option}
-                checked={field.value === option}
-                onChange={() => field.onChange(option)}
-                className={clsx(css.radioInput, css[theme])}
-              />
-              <span className={css.customRadio}></span>
-              {option}
-            </label>
-          ))}
+        <div className={css.wrapper}>
+          <span className={css.labelTitle}>Theme:</span>
+
+          <div className={css.radioGroup}>
+            {options.map((option) => (
+              <label key={option} className={css.radioLabel}>
+                <input
+                  type="radio"
+                  value={option}
+                  checked={field.value === option}
+                  onChange={() => field.onChange(option)}
+                  className={clsx(css.radioInput, css[option])}
+                />
+
+                <span className={clsx(css.customRadio, css[option])}></span>
+              </label>
+            ))}
+          </div>
         </div>
       )}
     />
